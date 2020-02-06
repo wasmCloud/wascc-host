d8d20248e39c549e4bb5ec8024dc5b4f5e2fd909
Brooks Townsend <brooksmtownsend@gmail.com>
2020-02-06 13:10:34 -0500

    Add replace UI using local nginx process (#8)
    * Add replace UI using local nginx process
    
    * fix name in package.json
    
    * Remove build files in favor of npm install dependency

---
+++ examples/replace_ui/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+
+set -euo pipefail
+
+DIR=$(pwd)
+
+sed "s,DIR,$DIR," < nginx.template > nginx.conf
+
+npm install
+npm run build # Only necessary if you've lost your static build files.
+
+cat $DIR/nginx.conf
+
+nginx -c $DIR/nginx.conf
