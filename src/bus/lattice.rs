f2099649ac5ade0296d661500be75f7718d039ca
Kevin Hoffman <alothien@gmail.com>
2020-06-11 12:32:48 -0400

    Switching to official NATS client for lattice, adding support for JWT credentials auth

--- src/bus/lattice.rs
+++ src/bus/lattice.rs
@@ -10,6 +10,7 @@ const LATTICE_HOST_KEY: &str = "LATTICE_HOST"; // env var name
 const DEFAULT_LATTICE_HOST: &str = "127.0.0.1"; // default mode is anonymous via loopback
 const LATTICE_RPC_TIMEOUT_KEY: &str = "LATTICE_RPC_TIMEOUT_MILLIS";
 const DEFAULT_LATTICE_RPC_TIMEOUT_MILLIS: u64 = 500;
+const LATTICE_CREDSFILE_KEY: &str = "LATTICE_CREDS_FILE";
 
 pub(crate) struct DistributedBus {
     nc: nats::Connection,
@@ -18,7 +19,8 @@ pub(crate) struct DistributedBus {
 
 impl DistributedBus {
     pub fn new() -> Self {
-        let nc = nats::connect(&get_env(LATTICE_HOST_KEY, DEFAULT_LATTICE_HOST)).unwrap();
+        let nc = get_connection();
+
         info!("Initialized Message Bus (lattice)");
         DistributedBus {
             nc,
@@ -75,6 +77,10 @@ fn invocation_from_msg(msg: &nats::Message) -> Invocation {
     i
 }
 
+fn get_credsfile() -> Option<String> {
+    std::env::var(LATTICE_CREDSFILE_KEY).ok()
+}
+
 fn get_env(var: &str, default: &str) -> String {
     match std::env::var(var) {
         Ok(val) => {
@@ -86,6 +92,17 @@ fn get_env(var: &str, default: &str) -> String {
         }
         Err(_) => default.to_string(),
     }
+}
+
+fn get_connection() -> nats::Connection {
+    if let Some(creds) = get_credsfile() {
+        nats::ConnectionOptions::new()
+            .with_credentials(creds)
+            .connect(&get_env(LATTICE_HOST_KEY, DEFAULT_LATTICE_HOST))
+    } else {
+        nats::ConnectionOptions::new().connect(&get_env(LATTICE_HOST_KEY, DEFAULT_LATTICE_HOST))
+    }
+    .unwrap()
 }
 
 fn get_timeout() -> Duration {
