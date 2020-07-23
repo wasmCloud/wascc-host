6877a97dc120baec541826e5b6b713a2963a6066
Kevin Hoffman <alothien@gmail.com>
2020-07-23 11:10:54 -0400

    Format

--- tests/integration_core.rs
+++ tests/integration_core.rs
@@ -4,7 +4,6 @@ mod common;
 use reqwest;
 use std::error::Error;
 
-
 #[test]
 fn stock_host() -> Result<(), Box<dyn Error>> {
     let host = common::gen_stock_host()?;
@@ -24,7 +23,7 @@ fn stock_host() -> Result<(), Box<dyn Error>> {
 
     let resp = reqwest::blocking::get("http://localhost:8081")?;
     assert!(resp.status().is_success());
-    assert_eq!(resp.text()?, 
+    assert_eq!(resp.text()?,
         "{\"method\":\"GET\",\"path\":\"/\",\"query_string\":\"\",\"headers\":{\"accept\":\"*/*\",\"host\":\"localhost:8081\"},\"body\":[]}"
     );
     host.shutdown()?;
@@ -33,28 +32,26 @@ fn stock_host() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
-fn kv_host() -> Result<(), Box<dyn Error>> {      
+fn kv_host() -> Result<(), Box<dyn Error>> {
     use redis::Commands;
 
     let host = common::gen_kvcounter_host(8083)?;
     std::thread::sleep(::std::time::Duration::from_millis(100));
     let key = uuid::Uuid::new_v4().to_string();
-    let rkey = format!(":{}", key);                 // the kv wasm logic does a replace on '/' with ':'
+    let rkey = format!(":{}", key); // the kv wasm logic does a replace on '/' with ':'
     let url = format!("http://localhost:8083/{}", key);
     let client = redis::Client::open("redis://127.0.0.1/")?;
-    let mut con = client.get_connection()?;    
+    let mut con = client.get_connection()?;
 
     let mut resp = reqwest::blocking::get(&url)?;
     assert!(resp.status().is_success());
     reqwest::blocking::get(&url)?;
     resp = reqwest::blocking::get(&url)?; // counter should be at 3 now
     assert!(resp.status().is_success());
-    assert_eq!(resp.text()?,
-        "{\"counter\":3}"
-    );
+    assert_eq!(resp.text()?, "{\"counter\":3}");
     host.shutdown()?;
     std::thread::sleep(::std::time::Duration::from_millis(100));
-    
-    let _:() = con.del(&rkey)?;
+
+    let _: () = con.del(&rkey)?;
     Ok(())
 }
